b524f01e4ae8336422fd318b562e5b0b71e2bcdd
Jorge Aranda <jorge.aranda@cuevano.ca>
2019-02-08 15:22:44 -0800

    Iterate on ruby Triangle exercise

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -6,30 +6,31 @@ class Triangle
   end
 
   def equilateral?
-    matching_side_pairs == 3 && sides.max > 0
+    legal_triangle? && sides.uniq.size == 1
   end
 
   def isosceles?
-    triangle_inequality? && matching_side_pairs >= 1
+    legal_triangle? && sides.uniq.size <= 2
   end
 
   def scalene?
-    triangle_inequality? && !isosceles?
+    legal_triangle? && !isosceles?
   end
 
   private
 
-  def triangle_inequality?
-    sides.permutation(3).reduce(true) {
-      |equal, perm|
-      equal && (perm[0] + perm[1] >= perm[2])
-    }
+  def legal_triangle?
+    at_least_one_positive_side? && !triangle_inequality_violations?
   end
 
-  def matching_side_pairs
-    sides.combination(2).reduce(0) {
-      |sum, pair|
-      sum + (pair[0] == pair[1] ? 1 : 0)
+  def at_least_one_positive_side?
+    sides.max > 0
+  end
+
+  def triangle_inequality_violations?
+    sides.permutation(3).to_a.any? {
+      |perm|
+      perm[0] + perm[1] < perm[2]
     }
   end
 end
