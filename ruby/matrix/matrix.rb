e6f7cf6ee2faabcc2b56dd281fb75327987e6b59
Jorge Aranda <jorge.aranda@cuevano.ca>
2019-02-08 20:51:00 -0800

    Tweak ruby Matrix solution

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,12 +2,16 @@ class Matrix
   attr_reader :rows
 
   def initialize(input)
-    @rows = input
-      .lines
-      .map { |row| row.split.map(&:to_i) }
+    @rows = input.lines.map { |row| parse(row) }
   end
 
   def columns
     rows.transpose
+  end
+
+  private
+
+  def parse(row)
+    row.split.map(&:to_i)
   end
 end
