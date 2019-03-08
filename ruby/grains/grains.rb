0794e955bb8c2ad3f10639a64316f1788768c506
Jorge Aranda <jorge.aranda@cuevano.ca>
2019-03-07 16:21:23 -0800

    Simplify the ruby Grains solution

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -2,16 +2,12 @@
 # given that the number on each square doubles.
 class Grains
   def self.square(num)
-    if num == 1
-      1
-    elsif num.between?(2, 64)
-      square(num - 1) * 2
-    else
-      raise ArgumentError, 'The argument must be equal to or greater than one'
-    end
+    raise ArgumentError if num < 1 || num > 64
+
+    2**(num - 1)
   end
 
   def self.total
-    (1..64).reduce { |sum, num| sum + square(num) }
+    2**64 - 1
   end
 end
