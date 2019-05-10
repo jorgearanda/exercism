e37016eba8a3ad148bbabf2b32ff79da4fcd514c
Jorge Aranda <jorge.aranda@cuevano.ca>
2019-05-10 15:12:54 -0700

    Refactor hamming exercise to a single function

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,12 +1,6 @@
 hamming <- function(strand1, strand2) {
-  nucleotides1 = get_nucleotides(strand1)
-  nucleotides2 = get_nucleotides(strand2)
-  if (length(nucleotides1) != length(nucleotides2)) {
+  if (nchar(strand1) != nchar(strand2)) {
     stop("Strands must be of the same length.")
   }
-  length(nucleotides1[nucleotides1 != nucleotides2])
-}
-
-get_nucleotides <- function(strand) {
-  unlist(strsplit(strand, ""))
+  sum(unlist(strsplit(strand1, "")) != unlist(strsplit(strand2, "")))
 }
